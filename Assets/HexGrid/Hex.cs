0382f706ebdf97d8a15f44e90051549d1e622033
John Vanderbeck <jwvanderbeck@gmail.com>
2014-12-05 16:58:47 -0800

    Initial commit

---
+++ Assets/HexGrid/Hex.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hex : MonoBehaviour {
+
+	// This class defines an individual hex on the grid
+	public float size;
+	public float level;
+	public Texture texture;
+
+	[HideInInspector]
+	public Vector2 axialCoordinates;
+	[HideInInspector]
+	public Vector2 worldCoordinates;
+	[HideInInspector]
+	public float height
+	{
+		get { return size * 2f; }
+	}
+	[HideInInspector]
+	public float width
+	{
+		get { return Mathf.Sqrt(3f)/2f * height; }
+	}
+	[HideInInspector]
+	public float hDistance
+	{
+		get { return width; }
+	}
+	[HideInInspector]
+	public float vDistance
+	{
+		get { return 3f/4f * height; }
+	}
+
+	Vector3 hexCenter;
+	Vector3[] vertices;
+	Vector2[] uvs;
+	int[] triangles;
+
+	public void DrawHex()
+	{
+		// Generate our vertices given a center and size
+		//      1
+		//     __ 
+		//  2 /  \ 0
+		//   |    |
+		//  3 \  / 5
+		//     --
+		//      4
+		hexCenter = this.transform.localPosition;
+		vertices = new Vector3[6];
+		for (int i = 0; i < 6; i++) 
+		{
+			float angle = 2.0f * Mathf.PI / 6.0f * (i + 0.5f);
+			float xPos = size * Mathf.Cos(angle);
+			float yPos = size * Mathf.Sin(angle);
+			vertices[i] = new Vector3(xPos, level, yPos);
+//			Debug.Log (xPos + "," + yPos);
+		}
+		
+		// Generate triangles connecting the vertices into a hexagon
+		// Vertices go clockwise
+		triangles = new int[]
+		{
+			1,0,5,
+			1,5,2,
+			2,5,4,
+			2,4,3
+		};
+		
+		// Apply UV coordinates
+		uvs = new Vector2[]
+		{
+			new Vector2(1.0f, 0.75f),
+			new Vector2(0.5f, 1.0f),
+			new Vector2(0.25f, 0.75f),
+			new Vector2(0.0f, 0.5f),
+			new Vector2(0.25f, 0.25f),
+			new Vector2(0.0f, 0.5f),
+		};
+		
+		// Create the Mesh pieces on our GameObject
+		// We need a Mesh Filter and a Mesh Renderer
+		MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		meshFilter.mesh = new Mesh();
+		meshFilter.mesh.vertices = vertices;
+		meshFilter.mesh.triangles = triangles;
+		meshFilter.mesh.uv = uvs;
+		meshFilter.mesh.RecalculateNormals();
+		// Apply texture for test rendering
+		renderer.material.mainTexture = texture;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/HexGrid/HexMap.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// A collection of multiple hexes to form a map
+public class HexMap : MonoBehaviour {
+	
+//	public int numberOfRows;
+	public int mapWidth;
+	public int numberOfLevels;
+	public float hexRadius;
+
+	[HideInInspector]
+	public Dictionary<string, Hex> tiles;
+	[HideInInspector]
+	public int[][] axialOffsets;
+
+	Hex GetHexAtAxialLocation(int q, int r)
+	{
+		// q = x, r = z
+		return tiles[q + "," + r];
+	}
+	void SetHexAtAxialLocation(int q, int r, Hex tile)
+	{
+		tiles[q + "," + r] = tile;
+	}
+	Hex Neighbour(Hex startingTile, int direction)
+	{
+		int qNeighbour = (int)startingTile.axialCoordinates.x + Direction(direction)[0];
+		int rNeighbour = (int)startingTile.axialCoordinates.y + Direction(direction)[1];
+		Hex neighbour = GetHexAtAxialLocation(qNeighbour, rNeighbour);
+		return neighbour;
+	}
+	Hex Neighbour(int[] startingLocation, int direction)
+	{
+		int qNeighbour = startingLocation[0] + Direction(direction)[0];
+		int rNeighbour = startingLocation[1] + Direction(direction)[1];
+		Hex neighbour = GetHexAtAxialLocation(qNeighbour, rNeighbour);
+		return neighbour;
+	}
+	int[] NeighbourPosition(int[] startingLocation, int direction)
+	{
+		int qNeighbour = startingLocation[0] + Direction(direction)[0];
+		int rNeighbour = startingLocation[1] + Direction(direction)[1];
+		return new int[]{qNeighbour, rNeighbour};
+	}
+	int[] NeighbourPosition(Hex startingTile, int direction)
+	{
+		int qNeighbour = (int)startingTile.axialCoordinates.x + Direction(direction)[0];
+		int rNeighbour = (int)startingTile.axialCoordinates.y + Direction(direction)[1];
+		return new int[]{qNeighbour, rNeighbour};
+	}
+	int[] Direction(int direction)
+	{
+		if (axialOffsets == null)
+		{
+			axialOffsets = new int[][]
+			{
+				new int[] {1,0},
+				new int[] {1,-1},
+				new int[] {0,-1},
+				new int[] {-1,0},
+				new int[] {-1,1},
+				new int[] {0,1}
+			};
+		}
+		if (direction < 0 || direction > 5)
+		{
+			return new int[] {0,0};
+		}
+		return axialOffsets[direction];
+	}
+	int[] Scale(int[] offset, int scale)
+	{
+		return new int[] { offset[0] * scale, offset[1] * scale };
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		// Init our tile storage
+		tiles = new Dictionary<string, Hex>();
+		// Compute map dimensions given a set width at center
+		// We compute to build a map that is the specified width at the center, and resembles an overall hexagon shape in total
+		// All maps must be odd tiles wide so that we have a nice center to work with
+		if (mapWidth % 2 == 0)
+			mapWidth += 1;
+
+		CreateHexAtAxialLocation(0,0);
+		for (int h = 1; h <= mapWidth; h++)
+		{
+			// walk out to first tile (southwest)
+			int[] offsets = Scale(Direction(4), h);
+//			Debug.Log ("Current Offsets = " + offsets[0] + "," + offsets[1]);
+			Hex currentTile = CreateHexAtAxialLocation(offsets[0], offsets[1]);
+//			Debug.Log ("Current Tile = " + currentTile.axialCoordinates.x + "," + currentTile.axialCoordinates.y);
+			// Starting from that tile, walk around in a ring - this is easily
+			// achieved by incrementing our direction 0-6 (We start with tile 4 because the first move after that is in direction 0
+			for (int d = 0; d < 6; d++)
+			{
+				for (int n = 0; n < h; n++)
+				{
+					int[] tileLocation = NeighbourPosition(currentTile, d);
+					currentTile = CreateHexAtAxialLocation(tileLocation[0], tileLocation[1]);
+				}
+			}
+		}
+	}
+	Hex CreateHexAtAxialLocation(int q, int r)	
+	{
+		GameObject goTile = new GameObject("Tile[" + q + "," + r + "]");
+		Hex tile = goTile.AddComponent("Hex") as Hex;
+		tile.size = hexRadius;
+		float xPos = (q * tile.hDistance) + ((r * tile.hDistance)/2);
+		float zPos = r * tile.vDistance * -1;
+		goTile.transform.position = new Vector3(xPos, 0, zPos);
+		tile.DrawHex();
+		tile.axialCoordinates.x = q;
+		tile.axialCoordinates.y = r;
+		SetHexAtAxialLocation(q, r, tile);
+		return tile;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
